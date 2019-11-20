7e282c483eb9a83529f39cddaefce6fa71e85579
ghcyguohai <624817436@qq.com>
2019-11-20 15:11:49 +0800

    去掉将rt-threadI当作SUBMODULE

---
+++ rt-thread/bsp/gd32e230k-start/RTE/_rt-thread_gd32e230/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'project' 
+ * Target:  'rt-thread_gd32e230' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
